f7c84d3d2bfa4feb1ca57a3a1c24f6f69fea9d5f
Tomás Arregui Bellino <ruidosocerebro@gmail.com>
2022-06-20 23:25:26 -0300

    Controller and route to get an user by name

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,9 @@ import { Router } from "express";
 
 const router = Router();
 import userRoute from "./user/getAllUser.routes";
+import nameRoute from "./user/getUserByName.routes"
 
 router.use('/user', userRoute);
+router.use('/name', nameRoute)
 
 export default router;
