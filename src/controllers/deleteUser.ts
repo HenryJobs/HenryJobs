c1031064e2c229e5f37cfc1bfe3d806a98c55c3f
Tomás Arregui Bellino <ruidosocerebro@gmail.com>
2022-06-21 16:50:08 -0300

    Controller to delete a user by it\s id

---
+++ src/controllers/deleteUser.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import { userModel, User } from "../models/User";
+import { userByNameInterface } from "./interfaces/userInterface";
+
+export const deleteUser = async (req: Request, res: Response) => {
+
+    const { id } = req.params;
+
+    try {
+
+        if (id) {
+            const deleteById = await userModel.findOneAndDelete({ id })
+
+            if ( deleteById) {
+                const myDelete: userByNameInterface = {
+                    _id: deleteById._id,
+                    firstName: deleteById.firstName,
+                    lastName: deleteById.lastName,
+                    userName: deleteById.userName,
+                    email: deleteById.email,
+                    password: deleteById.password
+                };
+
+                return res.status(200).json(myDelete)
+            };
+
+            res.status(404).json({ msg: "User does not exist" })
+        };
+        
+    } catch (error) {
+        console.error(error)       
+    };
+};
