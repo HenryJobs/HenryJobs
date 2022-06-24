26a8399ca00f79befe50837d88810e94f8fb98d2
Tomás Arregui Bellino <ruidosocerebro@gmail.com>
2022-06-24 20:01:26 -0300

    Added jwt validation

--- src/controllers/postUser.controller.ts
+++ src/controllers/postUser.controller.ts
@@ -4,6 +4,8 @@ import { userModel, User } from "../models/User";
 import { uploadImage } from "../cloudinary";
 import { UploadedFile } from "express-fileupload";
 
+import jwt from "jsonwebtoken";
+
 export const createUser = async (req: Request, res: Response, next: NextFunction) => {
 
     const { firstName,
@@ -15,17 +17,19 @@ export const createUser = async (req: Request, res: Response, next: NextFunction
     const { tempFilePath } = req.files?.profileImage as UploadedFile;
     const banner = req.files?.banner as UploadedFile;
 
+    
     try {
-
+        
         if (
             !firstName ||
             !lastName ||
             !userName ||
             !email ||
             !password
-        ) res.status(400).json({ msg: "Some fields are required" });
+            ) res.status(400).json({ msg: "Some fields are required" });
 
-        const user = await userModel.create({
+            
+            const user = await userModel.create({
             firstName,
             lastName,
             userName,
@@ -54,6 +58,13 @@ export const createUser = async (req: Request, res: Response, next: NextFunction
             await unlink(banner.tempFilePath)
         };
 
+        // crea un token y lo manda al header
+        const token: string = jwt.sign(
+            { _id: user._id },
+            process.env.TOKEN_SECRET || "TOKENTEST"
+        );
+
+        res.header("authToken", token).status(201).json(user)
 
         await user.save();
         res.status(201).json(user)
