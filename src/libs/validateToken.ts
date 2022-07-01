61d221e157d1e01c61469d157b722992c7fbcb43
Gabriel Cavasso <gaerca123@gmail.com>
2022-07-01 20:34:06 -0300

    tokenupdate

--- src/libs/validateToken.ts
+++ src/libs/validateToken.ts
@@ -6,19 +6,19 @@ import jwt from "jsonwebtoken";
 const { TOKEN_SECRET } = process.env;
 
 //Valida si el tipo del usuario es el correcto
-// const validateUserTypes = (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction,
-//   type: number
-// ) => {
-//   const { userType } = req;
-//   if (userType !== type) {
-//     return res.status(401).send("Unauthorized access is denied due to user");
-//   } else {
-//     return next();
-//   }
-// };
+const validateUserTypes = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+  type: number
+) => {
+  const { userType } = req;
+  if (userType !== type) {
+    return res.status(401).send("Unauthorized access is denied due to user");
+  } else {
+    return next();
+  }
+};
 
 export const tokenValidation = (
   req: Request,
@@ -50,57 +50,57 @@ export const tokenValidation = (
     console.error("Error JWT--->", error);
   }
 };
-// export const validatePremiumToken = (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   const { userPremium } = req;
+export const validatePremiumToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { userPremium } = req;
 
-//   if (!userPremium) {
-//     return res
-//       .status(401)
-//       .send("Unauthorized access is denied, you need to be premium");
-//   }
+  if (!userPremium) {
+    return res
+      .status(401)
+      .send("Unauthorized access is denied, you need to be premium");
+  }
 
-//   return next();
-// };
-// export const validatePGToken = (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   validateUserTypes(req, res, next, 1);
-// };
+  return next();
+};
+export const validatePGToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  validateUserTypes(req, res, next, 1);
+};
 
-// export const validateGraduateToken = (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   validateUserTypes(req, res, next, 2);
-// };
+export const validateGraduateToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  validateUserTypes(req, res, next, 2);
+};
 
-// export const validateRecruiterToken = (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   validateUserTypes(req, res, next, 3);
-// };
+export const validateRecruiterToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  validateUserTypes(req, res, next, 3);
+};
 
-// export const validateStaffToken = (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   validateUserTypes(req, res, next, 4);
-// };
+export const validateStaffToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  validateUserTypes(req, res, next, 4);
+};
 
-// export const validateBusinessToken = (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   validateUserTypes(req, res, next, 5);
-// };
+export const validateBusinessToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  validateUserTypes(req, res, next, 5);
+};
