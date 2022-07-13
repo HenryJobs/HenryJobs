2f6dfb96e21e0f5e1b08a7cd8904a6e9e40ad99e
Tomás Arregui Bellino <ruidosocerebro@gmail.com>
2022-07-13 06:59:42 -0300

    arreglé la ruta del uploadImageController

--- src/routes/user/uploadImage.routes.ts
+++ src/routes/user/uploadImage.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { uploadImageController } from "../../controllers/uploadImages.controller";
 
 
+
 const router = Router()
 
 router.post('/', uploadImageController)
